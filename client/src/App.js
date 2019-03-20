624878bd08cad33c9538de45dd682998031ac5d6
valentino <xvalentino13@gmail.com>
2019-03-19 21:24:01 -0600

    this is ready I think

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Router } from "@reach/router";
+import AllAlarms from "./pages/AllAlarms";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { ThemeProvider } from "mineral-ui/themes";
+
+const client = new ApolloClient({
+  uri: "http://localhost:60000/simple/v1/cjtgkw9dq000401182eh8dd05"
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <ThemeProvider>
+          <Router>
+            <AllAlarms path="/" />
+          </Router>
+        </ThemeProvider>
+      </ApolloProvider>
     );
   }
 }
---
+++ client/src/pages/AllAlarms.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import Text from "mineral-ui/Text";
+
+const ALL_ALARMS = gql`
+  {
+    allAlarms {
+      id
+      text
+    }
+  }
+`;
+
+export default () => {
+  return (
+    <>
+      <Text>Alarms</Text>
+      <Query query={ALL_ALARMS}>
+        {result => {
+          if (result.loading || !result.data) {
+            return <div>loading...</div>;
+          }
+          if (result.error) {
+            return <div>there was an error</div>;
+          }
+          return (
+            <ul>
+              {result.data.allAlarms.map(alarm => (
+                <li key={alarm.id}>
+                  {alarm.id} -- {alarm.text}
+                </li>
+              ))}
+            </ul>
+          );
+        }}
+      </Query>
+    </>
+  );
+};
